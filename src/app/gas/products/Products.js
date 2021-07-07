780bfa0f56775fd18f87811a09b264b40d23bccd
Yunus YILDIRIM <y.yildirim.ce@gmail.com>
2021-07-07 11:19:01 +0300

    added ProductSidebarContent

--- src/app/gas/products/Products.js
+++ src/app/gas/products/Products.js
@@ -1,19 +1,27 @@
-import FusePageCarded from '@fuse/core/FusePageCarded';
+import FusePageSimple from '@fuse/core/FusePageSimple';
 import withReducer from 'app/store/withReducer';
 import reducer from '../store';
 import ProductsHeader from './ProductsHeader';
 import ProductsTable from './ProductsTable';
+import ProductsSidebarContent from './ProductsSidebarContent';
+import { useRef } from 'react';
 
 function Products() {
+	const pageLayout = useRef(null);
 	return (
-		<FusePageCarded
+		<FusePageSimple
 			classes={{
-				content: 'flex',
-				contentCard: 'overflow-hidden',
-				header: 'min-h-72 h-72 sm:h-136 sm:min-h-136'
+				contentWrapper: 'p-0 sm:p-24 h-full',
+				content: 'flex flex-col h-full',
+				leftSidebar: 'w-256 border-0',
+				header: 'min-h-72 h-72 sm:h-136 sm:min-h-136',
+				wrapper: 'min-h-0'
 			}}
-			header={<ProductsHeader />}
+			header={<ProductsHeader pageLayout={pageLayout} />}
 			content={<ProductsTable />}
+			sidebarInner
+			leftSidebarContent={<ProductsSidebarContent/>}
+			ref={pageLayout}
 			innerScroll
 		/>
 	);
